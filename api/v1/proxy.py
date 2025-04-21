4591c54e4c1c9e8858a6e6bedd477a4eb8e35ef9
fenghan0430 <114984963+fenghan0430@users.noreply.github.com>
2025-04-21 19:52:30 +0800

    refactor(api): 重构代理接口并添加参数校验
    - 重构了 /proxy 端点的逻辑，优化了代码结构
    - 添加了对请求参数的校验，提高了数据的合法性和完整性
    - 改进了错误处理机制，使用 HTTPException 返回具体错误信息
    - 移除了 visitor 相关代码，放到其他接口
    - 优化了配置文件的读取和写入逻辑，提高了代码的可维护性

--- api/v1/proxy.py
+++ api/v1/proxy.py
@@ -1,10 +1,8 @@
+from hmac import new
 from math import e
 from typing import Dict, Type
-from fastapi import APIRouter
-
-from entity.apiProxy import RequestProxy
+from fastapi import APIRouter, HTTPException
 from entity.proxy import HTTPProxyConfig, HTTPSProxyConfig, STCPProxyConfig, SUDPProxyConfig, TCPMuxProxyConfig, TCPProxyConfig, UDPProxyConfig, XTCPProxyConfig
-from entity.visitor import STCPVisitorConfig, SUDPVisitorConfig, XTCPVisitorConfig
 from utils.ConfigManager import ConfigManager
 
 # 临时配置文件地址
@@ -29,56 +27,76 @@ PROXY_TYPE_MAP: Dict[str, Type] = {
     'tcpmux': TCPMuxProxyConfig,
 }
 
-VISITOR_TYPE_MAP: Dict[str, Type] = {
-    'stcp': STCPVisitorConfig,
-    'sudp': SUDPVisitorConfig,
-    'xtcp': XTCPVisitorConfig,
-}
-
 @router.get("/proxy")
 async def getProxy():
-    return config.load_config().model_dump(by_alias=True, exclude_unset=True)
+    """获取所有隧道信息
+
+    Returns:
+        _type_: _description_
+    """
+    client_config = config.load_config()
+    return_data = []
+    
+    if client_config.proxies == None:
+        return []
+    
+    for i in client_config.proxies:
+        return_data.append(i.model_dump(by_alias=True, exclude_none=True))
+    
+    return return_data
 
 @router.post("/proxy", status_code=201)
-async def newProxy(data: RequestProxy):
-    try:
-        if data.type_ in PROXY_TYPE_MAP.keys():
-            # 转为实体类，顺便验证合不合法
-            new_proxy_config = PROXY_TYPE_MAP[data.type_](**data.data)
-            # 检测名字是否重名，也许事应该前端也做一遍？
-            client_config = config.load_config()
-            if client_config.proxies == None:
-                client_config.proxies = []
-            for i in client_config.proxies:
-                if i.name == new_proxy_config.name:
-                    return {"status": "error", "message": f"名字{new_proxy_config.name}已经被占用"}
-                if data.type_ in ['tcp', 'udp']:
-                    if i.remotePort == new_proxy_config.remotePort: # type: ignore
-                        return {"status": "error", "message": f"端口{new_proxy_config.remotePort}已经被占用"}
-            # 添加隧道到配置文件
-            client_config.proxies.append(new_proxy_config)
-            config.save_config(client_config)
-            # 热加载配置文件
-            
-        elif data.type_ in VISITOR_TYPE_MAP.keys():
-            new_proxy_config = VISITOR_TYPE_MAP[data.type_](**data.data)
-            
-            client_config = config.load_config()
-            if client_config.visitors == None:
-                client_config.visitors = []
-            for i in client_config.visitors:
-                if i.name == new_proxy_config.name:
-                    return {"status": "error", "message": f"名字{new_proxy_config.name}已经被占用"}
-                if i.bindAddr != None and i.bindAddr == new_proxy_config.bindAddr: # 通过配置文件验证本地端口占用
-                    if i.bindPort == new_proxy_config.bindPort:
-                        return {"status": "error", "message": f"地址{new_proxy_config.bindAddr}:{new_proxy_config.bindPort}已经被占用"}
-            
-            client_config.visitors.append(new_proxy_config)
-            config.save_config(client_config)
-        else:
-            return {"status": "error", "message": f"类型{data.type_}不是一个有效的类型"}
-    except Exception as e:
-        print(e)
-        return {"status": "error", "message": str(e)} # TODO 应返回错误码
+async def newProxy(data: dict):
+    """_summary_
 
-    return {"status": "ok"}
+    Args:
+        data (RequestProxy): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    
+    if "type" not in data.keys():
+        raise HTTPException(status_code=400, detail={"status": 400, "massage": "请求体中缺少type字段"})
+    
+    if data["type"] not in PROXY_TYPE_MAP.keys():
+        raise HTTPException(status_code=400, detail={"status": 400, "massage": "类型%s不是一个有效的类型"%data["type"]})
+    
+    try:
+        # 转为实体类，顺便验证合不合法
+        new_proxy_config = PROXY_TYPE_MAP[data["type"]](**data)
+        print(new_proxy_config.model_dump(exclude_none=True, by_alias=True))
+    except Exception as e:
+        raise HTTPException(status_code=422, detail={"status": 422, "massage": "配置文件格式不正确: " + str(e)})
+    
+    # 检测
+    client_config = config.load_config()
+    if client_config.proxies == None:
+        client_config.proxies = []
+    
+    for i in client_config.proxies:
+        # 检测名字是否重名，也许事应该前端也做一遍？
+        if i.name == new_proxy_config.name:
+            raise HTTPException(status_code=409, detail={"status": 409, "massage": f"名字{new_proxy_config.name}已经被占用"})
+        # 检测tcp, udp端口是否重复
+        if data["type"] in ['tcp', 'udp'] and i.type_ in ['tcp', 'udp']:
+            if new_proxy_config.remotePort == None:
+                raise HTTPException(status_code=422, detail={"status": 422, "massage": "remotePort为空,不支持这样的写法"})
+            if i.remotePort == new_proxy_config.remotePort:  # type: ignore
+                raise HTTPException(status_code=409, detail={"status": 409, "massage": f"端口{new_proxy_config.remotePort}已经被占用"})
+        # 检测http, https绑定域名是否重复
+        if data["type"] in ['http', 'https'] and i.type_ in ['http', 'https']:
+            if i.customDomains == None: # type: ignore
+                continue
+            if new_proxy_config.customDomains == None:
+                raise HTTPException(status_code=422, detail={"status": 422, "massage": "customDomains为空,不支持这样的写法"})
+            for domain in i.customDomains: # type: ignore
+                for domain2 in new_proxy_config.customDomains: # type: ignore
+                    if domain == domain2:
+                        return {"status": "error", "message": f"域名{domain2}已经被{i.name}占用"}
+    # 添加隧道到配置文件
+    client_config.proxies.append(new_proxy_config)
+    config.save_config(client_config)
+    # 热加载配置文件
+
+    return {"status": 200, "message": f"创建{new_proxy_config.type_}隧道{new_proxy_config.name}成功"}
