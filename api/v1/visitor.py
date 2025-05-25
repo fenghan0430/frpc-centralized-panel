48b9f5cac71406259814b9d84260ae98a0eda275
fenghan0430 <114984963+fenghan0430@users.noreply.github.com>
2025-05-25 19:14:49 +0800

    feat: 添加多客户端visitor配置文件支持

--- api/v1/visitor.py
+++ api/v1/visitor.py
@@ -1,8 +1,11 @@
+import os
 from typing import Dict, Type
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Path
 from entity.visitor import STCPVisitorConfig, SUDPVisitorConfig, XTCPVisitorConfig
+from utils import database
 from utils.ConfigManager import ConfigManager
+from utils.database import DataBase
 
 VISITOR_TYPE_MAP: Dict[str, Type] = {
     'stcp': STCPVisitorConfig,
@@ -11,36 +14,132 @@ VISITOR_TYPE_MAP: Dict[str, Type] = {
 }
 
 # 临时配置文件地址
-config_file = "temp_tool/test_config.toml"
+database_path = "data/data.db"
 #
 
-config = ConfigManager(config_file)
 
 router = APIRouter(
     prefix="/api/v1",
     responses={404: {"description": "Not found"}},
 )
 
-@router.get("/visitor")
-async def getProxy():
-    """获取所有接收配置
+def verify_client_id(client_id: int):
+    """验证客户端ID是否存在
+    
+    Args:
+        client_id (str): 客户端ID
+    
+    Raises:
+        HTTPException:
+            - 500: 数据库查询失败
+            - 400: 客户端ID不存在
+            - 400: 客户端ID对应的frpc.toml文件不存在
+    """
+    # 数据库验证id存不存在, 防止未知id
+    # 从数据库得到一个可信的id列表
+    try:
+        with DataBase(database_path) as db:
+            results = db.query_program()
+            db_id = [result[0] for result in results]
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail={"status": 500, "message": f"数据库查询失败: {str(e)}"}
+        )
+    
+    if client_id not in db_id:
+        raise HTTPException(status_code=400, detail={"status": 404, "message": "无效的客户端ID"})
+    
+    if not os.path.isfile(f"data/cmd/{client_id}/frpc.toml"):
+        raise HTTPException(status_code=400, detail={"status": 404, "message": "客户端ID对应的frpc.toml文件不存在"})
+    
+    return True
+
+def get_all_visitor():
+    """获取所有代理配置
+
+    Raises:
+        HTTPException: 
+            - 500: 数据库查询失败
+            - 500: 无法读取配置文件
 
     Returns:
-        _type_: _description_
+        List: 所有代理配置列表
     """
-    client_config = config.load_config()
-    return_data = []
+    # 数据库验证id存不存在, 防止未知id
+    # 从数据库得到一个可信的id列表
+    try:
+        with DataBase(database_path) as db:
+            results = db.query_program()
+            db_id = [str(result[0]) for result in results]
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail={"status": 500, "message": f"数据库查询失败: {str(e)}"}
+        )
     
-    if client_config.visitors == None:
+    if not db_id:
+        return []
+
+    all_visitors = []
+    
+    # 得到data/cmd/下的文件夹
+    cmd_path = os.path.join("data", "cmd")
+    if not os.path.exists(cmd_path):
         return []
     
-    for i in client_config.visitors:
-        return_data.append(i.model_dump(by_alias=True, exclude_none=True))
+    try:
+        folder_names = [f for f in os.listdir(cmd_path) if os.path.isdir(os.path.join(cmd_path, f))]
+    except OSError as e:
+        raise HTTPException(
+            status_code=500, 
+            detail={"status": 500, "message": f"无法读取配置文件夹: {str(e)}"}
+            )
     
-    return return_data
+    for folder_name in folder_names:
+        # 验证文件夹名在不在db id, 如不在，跳过
+        if folder_name not in db_id:
+            # TODO: log
+            continue
+        
+        # 验证文件夹下有没有frpc.toml, 如没有，跳过
+        config_path = os.path.join(cmd_path, folder_name, "frpc.toml")
+        if not os.path.exists(config_path):
+            # TODO: log
+            continue
+        
+        # 加载config, 如果错误，跳过
+        try:
+            client_config_manager = ConfigManager(config_path)
+            client_config = client_config_manager.load_config()
+        except Exception as e:
+            # TODO: log
+            print(str(e))
+            continue
+        
+        # 检查config.proxies是否为空, 如为空，跳过
+        if not client_config.visitors:
+            continue
+        
+        # 读取每一条proxy，添加上client_id, 值为cid
+        for visitor in client_config.visitors:
+            visitor_dict = visitor.model_dump(by_alias=True, exclude_none=True)
+            visitor_dict["client_id"] = int(folder_name)
+            # 添加到all_proxies
+            all_visitors.append(visitor_dict)
+        
+    return all_visitors
 
-@router.get("/visitor/{visitor_name}", status_code=200)
-async def get_visitor_by_name(visitor_name: str):
+@router.get("/visitor")
+async def getVisitor():
+    """获取所有接收配置"""
+    return get_all_visitor()
+
+@router.get("/visitor/{client_id}/{visitor_name}", status_code=200)
+async def get_visitor_by_name(
+    visitor_name: str = Path(..., title="配置名"),
+    client_id: int = Path(..., title="客户端ID")
+    ):
     """
     获取指定 name 的接收配置
 
@@ -50,7 +149,10 @@ async def get_visitor_by_name(visitor_name: str):
     Returns:
         dict: 接收配置信息
     """
-    client_config = config.load_config()
+    verify_client_id(client_id)
+    
+    # TODO: 这个接口下的加载参数是不是该加try
+    client_config = ConfigManager(f"data/cmd/{client_id}/frpc.toml").load_config()
     
     # 没有任何接收配置
     if not client_config.visitors:
@@ -70,8 +172,14 @@ async def get_visitor_by_name(visitor_name: str):
         detail={"status": 404, "message": f"未找到名为 {visitor_name} 的接收配置"}
     )
 
-@router.post("/visitor", status_code=201)
-def newVisitor(data: dict):
+@router.post("/visitor/{client_id}", status_code=201)
+def newVisitor(
+    data: dict,
+    client_id: int = Path(..., description="客户端ID"),
+    ):
+    verify_client_id(client_id)
+    
+    config = ConfigManager(f"data/cmd/{client_id}/frpc.toml")
     
     if "type" not in data.keys():
         raise HTTPException(status_code=400, detail={"status": 400, "message": "请求体中缺少type字段"})
@@ -107,8 +215,11 @@ def newVisitor(data: dict):
         
         return {"status": 201, "message": "接收配置%s创建成功"%new_visitor_config.name}
 
-@router.patch("/visitor", status_code=200)
-async def update_visitor(data: dict):
+@router.patch("/visitor/{client_id}", status_code=200)
+async def update_visitor(
+    data: dict,
+    client_id: int = Path(..., description="客户端ID"),
+    ):
     """
     更新指定 name 的接收配置
 
@@ -119,6 +230,10 @@ async def update_visitor(data: dict):
         dict: 更新结果状态信息
     """
 
+    verify_client_id(client_id)
+    
+    config = ConfigManager(f"data/cmd/{client_id}/frpc.toml")
+    
     if "type" not in data.keys():
         raise HTTPException(status_code=400, detail={"status": 400, "message": "请求体中缺少type字段"})
 
@@ -172,8 +287,11 @@ async def update_visitor(data: dict):
 
     return {"status": 200, "message": f"更新接收配置 {visitor_name} 成功"}
 
-@router.delete("/visitor/{visitor_name}", status_code=200)
-async def delete_visitor(visitor_name: str):
+@router.delete("/visitor/{client_id}/{visitor_name}", status_code=200)
+async def delete_visitor(
+    visitor_name: str = Path(..., description="配置名"),
+    client_id: int = Path(..., description="客户端ID"),
+    ):
     """删除指定的接收配置
 
     Args:
@@ -182,6 +300,10 @@ async def delete_visitor(visitor_name: str):
     Returns:
         dict: 删除结果的状态信息
     """
+    verify_client_id(client_id)
+    
+    config = ConfigManager(f"data/cmd/{client_id}/frpc.toml")
+    
     client_config = config.load_config()
     
     if client_config.visitors is None or not client_config.visitors:
